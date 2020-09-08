f0caa94d51e37fa7d47d6ac4b7bae27e5e71ad4c
Producciones AG <desarrolloweb@agvideo.tv>
2020-09-08 13:48:18 -0500

    guardando preferencias compartidas en archivos xml

--- app/src/main/java/com/dalda/mycontact/EditActivity.java
+++ app/src/main/java/com/dalda/mycontact/EditActivity.java
@@ -1,15 +1,29 @@
 package com.dalda.mycontact;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class EditActivity extends AppCompatActivity {
+
+    private static final int CODIGO_SOLICITUD_PERMISO = 1;
+    private static final int CODIGO_SOLICITUD_HABILITAR_BLUETOOTH = 0;
+    private Context context;
+    private Activity activity;
     private  String campo_vacio ="Campo Vacio";
     public Contacto contact;
 
@@ -17,6 +31,11 @@ public class EditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        /*start inicializando variables para el premiso de bluetooth*/
+        context = getApplicationContext();
+        activity = this;
+        /*end inicializando variables para el premiso de bluetooth*/
 
         Intent intent = getIntent();
 
@@ -44,4 +63,50 @@ public class EditActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
+    /*start inicializando variables para el premiso de bluetooth*/
+    public void habilitarBluetooth(View v){
+        solicitarPermiso();
+        BluetoothAdapter mBluetooAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetooAdapter == null){
+            Toast.makeText(EditActivity.this, "Ti dispositivo no tiene bluetooth", Toast.LENGTH_SHORT).show();
+        }
+        if (!mBluetooAdapter.isEnabled()){
+            Intent habilitarBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(habilitarBluetoothIntent, CODIGO_SOLICITUD_HABILITAR_BLUETOOTH);
+        }
+    }
+
+    public boolean checarStatusPermiso(){
+        int resultado = ContextCompat.checkSelfPermission(context, Manifest.permission.BLUETOOTH);
+        if (resultado == PackageManager.PERMISSION_GRANTED){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void solicitarPermiso(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.BLUETOOTH)){
+            Toast.makeText(EditActivity.this, "el permiso ya fue otorgado, si deseas desactivarlo, puedes ir a los ajustes de la aplicacion", Toast.LENGTH_SHORT).show();
+        }else{
+            ActivityCompat.requestPermissions(activity, new String[] {Manifest.permission.BLUETOOTH}, CODIGO_SOLICITUD_PERMISO);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case CODIGO_SOLICITUD_PERMISO:
+                if (checarStatusPermiso()){
+                    Toast.makeText(EditActivity.this, "Ya esta activo el permiso para bluetooth", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(EditActivity.this, "No esta activo el permiso para bluetooth", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    /*end inicializando variables para el premiso de bluetooth*/
 }
--- app/src/main/java/com/dalda/mycontact/MainActivity.java
+++ app/src/main/java/com/dalda/mycontact/MainActivity.java
@@ -3,12 +3,19 @@ package com.dalda.mycontact;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
     private String nombre = "";
@@ -33,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void saveContact(View view) {
-        Intent intent = new Intent(this, EditActivity.class);
         EditText editTextnombre = (EditText) findViewById(R.id.editTextTextPersonName);
         nombre = editTextnombre.getText().toString();
 
@@ -53,14 +59,40 @@ public class MainActivity extends AppCompatActivity {
         EditText editTextdescripcion = (EditText) findViewById(R.id.editTextTextMultiLine);
         descripcion = editTextdescripcion.getText().toString();
 
+        SharedPreferences miPrefenciaCompartida = getSharedPreferences("MisDatosPersonales", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = miPrefenciaCompartida.edit();
+
+        editor.putString("nombre", nombre);
+        editor.putString("correo", email);
+
+        editor.commit();
+
+        Toast.makeText(MainActivity.this, "Se ha creado la preferencia compartida", Toast.LENGTH_SHORT).show();
+        editTextnombre.setText("");
+        editTextemail.setText("");
+
+        /*abajo codigo para enviar datos a otra activity y cambiar de activity */
+       /*
+        Intent intent = new Intent(this, EditActivity.class);
         contac = new Contacto(nombre, nacimiento, telefono, email, descripcion, year, month, day);
         intent.putExtra("mycontact", contac);
         startActivity(intent);
-        finish();
+        finish();*/
+    }
+
+    public void mostrarPrefencia(View view) {
+        SharedPreferences miPrefenciaCompartida = getSharedPreferences("MisDatosPersonales", Context.MODE_PRIVATE);
+
+        nombre = miPrefenciaCompartida.getString("nombre", "no existe esta varable");
+        email = miPrefenciaCompartida.getString("correo", "no existe esta varable");
+        String preferencia = "\nNombre: " + nombre + "\nCorreo: " +email;
+        TextView tvPrefernciaCompartida = (TextView) findViewById(R.id.tvPrefernciaCompartida);
+        tvPrefernciaCompartida.setText(preferencia);
     }
 
     public void editandoContac(){
         Intent intent = getIntent();
+
         contac = (Contacto)intent.getSerializableExtra("myeditar");
         if (contac != null) {
             EditText tv_nombre = findViewById(R.id.editTextTextPersonName);
@@ -80,6 +112,23 @@ public class MainActivity extends AppCompatActivity {
 
             EditText tv_descripcion = findViewById(R.id.editTextTextMultiLine);
             tv_descripcion.setText(contac.getDescripcion());
+        }
+    }
+
+    public void generarArchivo(View v){
+        try {
+            EditText editTextnombre = (EditText) findViewById(R.id.editTextTextPersonName);
+            nombre = editTextnombre.getText().toString();
+
+            FileOutputStream outputStream = null;
+            outputStream = openFileOutput("MiArchivo.txt", Context.MODE_APPEND);
+            outputStream.write(nombre.getBytes());
+            outputStream.close();
+            Toast.makeText(MainActivity.this, "El archivo se ha creado", Toast.LENGTH_SHORT).show();
+            editTextnombre.setText("");
+        }catch (Exception e){
+            e.printStackTrace();
+            Toast.makeText(MainActivity.this, "Hubo un error en la escritura del archivo", Toast.LENGTH_SHORT).show();
         }
     }
 }
